b623912f171cb736f967f369d9c5a1691973e4cc
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-03-21 13:40:14 +0000

    [update]public/cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,8 @@
 class Public::CartItemsController < ApplicationController
   def index
+    # @cart_items = current_customer.cart_items
+    # @tax_in_price = menus.non_tax_price * 1.1
+    # @total_price = subtotal * count
   end
 
   def destroy_all
