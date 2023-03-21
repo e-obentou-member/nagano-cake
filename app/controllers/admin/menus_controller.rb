32aa69d7617d6a6fc0d395cc4a0fcbbb89aeae3e
EC2 Default User <ec2-user@ip-172-31-18-41.ap-northeast-1.compute.internal>
2023-03-21 04:48:47 +0000

    [Add]indexページ作成

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -1,20 +1,30 @@
 class Admin::MenusController < ApplicationController
   def new
+    @menu = Menu.new
   end
 
   def index
+    @menus = Menu.all
   end
 
   def create
+    @menu = Menu.new(menu_params)
+    @menu.save
+    redirect_to menu_path
   end
 
   def show
+    @menu = Menu.find(params[:id])
   end
 
   def edit
+    @menu = Menu.find(params[:id])
   end
 
   def update
+    @menu = Menu.find(params[:id])
+    @menu.update
+    redirect_to menu_path(@menu.id)
   end
 
   private
