3937d8a1483bbfaf5343cc5b75db0ddda4ed6b57
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-03-22 06:12:21 +0000

    [update]public/menus

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
-    # menus.rbでアソシエーション記述済
-    # @manus = Menu.order(created_at: :desc)
+    # menus.rbでアソシエーション記述済(a)
+    @manus = Menu.order(created_at: :desc)
+    @genres = Menu.genre_id
   end
 
   def about
