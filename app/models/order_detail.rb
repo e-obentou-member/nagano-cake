18c276bf74e676b9b22b361b0de83132a9b194cd
Masuda-Jion <masuda0311jion@gmail.com>
2023-03-22 08:39:59 +0000

    [add] customer

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,12 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :menu
+  
+  enum make_status: {
+    not_startable: 0,
+    wating_for_production: 1,
+    under_production: 2,
+    production_completed: 3
+}
+  
 end
