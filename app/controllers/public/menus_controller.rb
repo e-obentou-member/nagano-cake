dfe52f3c92beb39d99619334acd01c5441b03060
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-03-23 01:37:58 +0000

    [upadate]public/menus

--- app/controllers/public/menus_controller.rb
+++ app/controllers/public/menus_controller.rb
@@ -2,10 +2,12 @@ class Public::MenusController < ApplicationController
   def index
     @menus = Menu.all
     @menus = Menu.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
     @menu = Menu.find(params[:id])
+    @genres = Genre.all
     @count = params[:count] || 1
     @cart_item = CartItem
   end
