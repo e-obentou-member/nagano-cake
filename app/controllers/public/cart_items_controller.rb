5983b5c3d62b7215ab0563111dc7e63cf86ba933
EC2 Default User <ec2-user@ip-172-31-7-225.ap-northeast-1.compute.internal>
2023-03-23 05:39:54 +0000

    [update]public/cart_items

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,9 +3,7 @@ class Public::CartItemsController < ApplicationController
 
 
   def index
-    # @cart_items = current_customer.cart_items.all
     @cart_items = current_customer.cart_items
-    subtotal = menu.tax_in_price * count
   end
 
   def create
@@ -22,21 +20,23 @@ class Public::CartItemsController < ApplicationController
 
     # もしカート内に「同じ」商品がない場合は通常の保存処理(a)
     elsif @cart_item.save
-      @cart_items = current_customer.cart_items.all
-      render 'index'
-    else　# 保存できなかった場合(a)
+      redirect_to cart_items_path
+    else  # 保存できなかった場合(a)
       render 'index'
     end
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
 
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
-    redirect_to request.referer
+    redirect_to cart_items_path
 
   end
 
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,19 +3,10 @@ class CartItem < ApplicationRecord
   belongs_to :menu
 
 
-  has_one_attached :image
-
-  def get_image(width, height)
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
-    end
-    image.variant(resize_to_limit: [width, height]).processed
+  # 小計計算(a)
+  def subtotal_price
+    menu.tax_in_price * self.count
   end
 
-  # 消費税計算(a)
-  def tax_in_price
-     (self.non_tax_price * 1.1).round
-  end
 
 end
