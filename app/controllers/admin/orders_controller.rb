83fc7d63f4ba22aa7c5792002513d389c435f7dc
Masuda-Jion <masuda0311jion@gmail.com>
2023-03-25 17:20:13 +0000

    [add] customer

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,9 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
+
   def show
+    # @cart_items = current_customer.cart_items.all
+    # @total_price =  @cart_items.sum(&:subtotal_price)
+    @postage = 800
   end
 end
