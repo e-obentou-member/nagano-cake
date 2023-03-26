879298c158ffb1a0c1556c9c5b8935c3fefbc98f
Masuda-Jion <masuda0311jion@gmail.com>
2023-03-26 05:28:34 +0000

    [add] customer

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     @orders = Order.all.page(params[:page]).per(1)
+    @order_details = OrderDetail.all
   end
 end
